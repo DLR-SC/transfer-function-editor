da48e542d8b0143a3c819eba6ac9c19cfcd87cdf
Jonas Gilg <jonas.gilg@dlr.de>
2023-03-02 12:49:30 +0100

    :wrench: Improving mouse interactions.

--- src/TransferFunctionEditor.ts
+++ src/TransferFunctionEditor.ts
@@ -21,24 +21,31 @@ export class TransferFunctionEditor {
     }
 }
 
+export interface AlphaStop {
+    stop: number;
+    alpha: number;
+}
+
 export class TransparencyEditor {
     private container: HTMLElement;
     private readonly canvas: HTMLCanvasElement;
     private ctx: CanvasRenderingContext2D;
-    private controlPoints: Array<{ x: number; alpha: number }>;
-    private colorMap: Array<{ x: number, rgb: string }> = [
-        {x: 0, rgb: 'blue'},
-        {x: 0.5, rgb: 'white'},
-        {x: 1, rgb: 'red'}
+    private controlPoints: Array<AlphaStop>;
+    private colorMap: Array<ColorStop> = [
+        {stop: 0, rgb: 'blue'},
+        {stop: 0.5, rgb: 'white'},
+        {stop: 1, rgb: 'red'}
     ];
 
     private isDragging: boolean = false;
     private dragIndex: number = -1;
+    private controlPointSize: number = 5;
 
-    constructor(container: HTMLElement | string, transferFunction: Array<{ x: number; alpha: number }> = [
-        {x: 0, alpha: 1},
-        {x: 0.5, alpha: 0.5},
-        {x: 1, alpha: 0}
+
+    constructor(container: HTMLElement | string, transferFunction: Array<AlphaStop> = [
+        {stop: 0, alpha: 1},
+        {stop: 0.5, alpha: 0.5},
+        {stop: 1, alpha: 0}
     ]) {
         if (container) {
             if (typeof (container) == "string") {
@@ -63,15 +70,15 @@ export class TransparencyEditor {
         this.addEventListeners();
     }
 
-    public getRGB(x: number): string {
-        let cp1: {x: number, rgb: string};
-        let cp2: {x: number, rgb: string};
+    public getRGB(stop: number): string {
+        let cp1: ColorStop;
+        let cp2: ColorStop;
         for (let i = 0; i < this.colorMap.length; i++) {
-            if (this.colorMap[i].x === x) {
+            if (this.colorMap[i].stop === stop) {
                 return this.colorMap[i].rgb;
             }
 
-            if (x < this.colorMap[i].x) {
+            if (stop < this.colorMap[i].stop) {
                 cp1 = this.colorMap[i - 1];
                 cp2 = this.colorMap[i];
                 break;
@@ -79,27 +86,27 @@ export class TransparencyEditor {
         }
 
         if (!cp1 || !cp2) {
-            throw "Parameter 'x' is not in the range [0, 1]!";
+            throw "Parameter 'stop' is not in the range [0, 1]!";
         }
 
         const color1 = cp1.rgb;
         const color2 = cp2.rgb;
-        const x1 = cp1.x;
-        const x2 = cp2.x;
+        const x1 = cp1.stop;
+        const x2 = cp2.stop;
 
-        return d3Interpolate.interpolateRgb(color1, color2)((x - x1) / (x2 - x1));
+        return d3Interpolate.interpolateRgb(color1, color2)((stop - x1) / (x2 - x1));
     }
 
-    public getAlpha(x: number): number {
+    public getAlpha(stop: number): number {
         // find control points
-        let cp1: {x: number, alpha: number};
-        let cp2: {x: number, alpha: number};
+        let cp1: AlphaStop;
+        let cp2: AlphaStop;
         for (let i = 0; i < this.controlPoints.length; i++) {
-            if (this.controlPoints[i].x === x) {
+            if (this.controlPoints[i].stop === stop) {
                 return this.controlPoints[i].alpha;
             }
 
-            if (x < this.controlPoints[i].x) {
+            if (stop < this.controlPoints[i].stop) {
                 cp1 = this.controlPoints[i - 1];
                 cp2 = this.controlPoints[i];
                 break;
@@ -107,45 +114,63 @@ export class TransparencyEditor {
         }
 
         if (!cp1 || !cp2) {
-            throw "Parameter 'x' is not in the range [0, 1]!";
+            throw "Parameter 'stop' is not in the range [0, 1]!";
         }
 
         const alpha1 = cp1.alpha;
         const alpha2 = cp2.alpha;
-        const x1 = cp1.x;
-        const x2 = cp2.x;
+        const x1 = cp1.stop;
+        const x2 = cp2.stop;
 
-        return d3Interpolate.interpolateNumber(alpha1, alpha2)((x - x1) / (x2 - x1));
+        return d3Interpolate.interpolateNumber(alpha1, alpha2)((stop - x1) / (x2 - x1));
     }
 
-    public getRGBA(x: number): string {
-        const color = d3Color.rgb(this.getRGB(x));
-        const a = this.getAlpha(x);
+    public getRGBA(stop: number): string {
+        const color = d3Color.rgb(this.getRGB(stop));
+        const a = this.getAlpha(stop);
         color.opacity = 1 - a;
         return color.formatHex8();
     }
 
+    public addControlPointAt(x, alpha) {
+        this.controlPoints.push({stop: x, alpha});
+        this.sortControlPoints();
+        this.draw();
+    }
+
+    public removeControlPointAt(x, y) {
+
+    }
+
+    public moveControlPointTo() {
+
+    }
+
     private draw() {
+        // Clear the drawing area.
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        const polygon = [{x: 0, alpha: 1}, ...this.controlPoints, {x: 1, alpha: 1}];
+        // Create a polygon of the area below the control points.
+        const polygon = [{stop: 0, alpha: 1}, ...this.controlPoints, {stop: 1, alpha: 1}];
         const gradient = this.ctx.createLinearGradient(0, this.canvas.height, this.canvas.width, this.canvas.height);
 
+        // If a color map exists, we fill the gradient with it, otherwise we just draw a black-alpha gradient.
         if (this.colorMap && this.colorMap.length > 0) {
-            const gradientStops = [...this.colorMap.map((entry) => entry.x), ...this.controlPoints.map((entry) => entry.x)].sort();
+            const gradientStops = [...this.colorMap.map((entry) => entry.stop), ...this.controlPoints.map((entry) => entry.stop)].sort();
             for (const x of gradientStops) {
                 gradient.addColorStop(x, this.getRGBA(x));
             }
         } else {
             for (const controlPoint of this.controlPoints) {
-                gradient.addColorStop(controlPoint.x, `rgba(0, 0, 0, ${1 - controlPoint.alpha})`);
+                gradient.addColorStop(controlPoint.stop, `rgba(0, 0, 0, ${1 - controlPoint.alpha})`);
             }
         }
 
+        // Draw the gradient.
         this.ctx.fillStyle = gradient;
         this.ctx.beginPath();
         for (let i = 0; i < polygon.length; i++) {
-            const x = polygon[i].x * this.canvas.width;
+            const x = polygon[i].stop * this.canvas.width;
             const y = polygon[i].alpha * this.canvas.height;
             if (i === 0) {
                 this.ctx.moveTo(x, y);
@@ -156,10 +181,11 @@ export class TransparencyEditor {
         this.ctx.closePath();
         this.ctx.fill();
 
+        // Draw the lines between points.
         this.ctx.strokeStyle = "black";
         this.ctx.beginPath();
         for (let i = 0; i < this.controlPoints.length; i++) {
-            const x = this.controlPoints[i].x * this.canvas.width;
+            const x = this.controlPoints[i].stop * this.canvas.width;
             const y = this.controlPoints[i].alpha * this.canvas.height;
             if (i === 0) {
                 this.ctx.moveTo(x, y);
@@ -169,15 +195,30 @@ export class TransparencyEditor {
         }
         this.ctx.stroke();
 
+        // Draw the control points.
         this.ctx.fillStyle = "white";
         for (let i = 0; i < this.controlPoints.length; i++) {
-            const x = this.controlPoints[i].x * this.canvas.width;
+            const x = this.controlPoints[i].stop * this.canvas.width;
             const y = this.controlPoints[i].alpha * this.canvas.height;
             this.ctx.beginPath();
-            this.ctx.arc(x, y, 5, 0, 2 * Math.PI);
+            this.ctx.arc(x, y, this.controlPointSize, 0, 2 * Math.PI);
             this.ctx.fill();
             this.ctx.stroke();
         }
+    }
+
+    private sortControlPoints() {
+        this.controlPoints.sort((a, b) => a.stop - b.stop);
+    }
+
+    private controlPointAt(stop: number, alpha: number) {
+
+    }
+
+    private pixelToNormalized(x: number, y: number): { stop: number, alpha: number } {
+        const stop = Math.max(0, Math.min(1, x / this.canvas.width));
+        const alpha = Math.max(0, Math.min(1, y / this.canvas.height));
+        return {stop, alpha};
     }
 
     private addEventListeners() {
@@ -185,9 +226,9 @@ export class TransparencyEditor {
             this.dragIndex = -1;
             for (let i = 0; i < this.controlPoints.length; i++) {
                 const controlPoint = this.controlPoints[i];
-                const dx = controlPoint.x * this.canvas.width - e.offsetX;
+                const dx = controlPoint.stop * this.canvas.width - e.offsetX;
                 const dy = controlPoint.alpha * this.canvas.height - e.offsetY;
-                if (Math.sqrt(dx * dx + dy * dy) < 5) {
+                if (Math.sqrt(dx * dx + dy * dy) < this.controlPointSize) {
                     this.dragIndex = i;
                     this.isDragging = true;
                     break;
@@ -207,18 +248,15 @@ export class TransparencyEditor {
             if (e.button === 0) { // Left click
                 const x = Math.max(0, Math.min(1, e.offsetX / this.canvas.width));
                 const y = Math.max(0, Math.min(1, e.offsetY / this.canvas.height));
-                const controlPoint = {x, alpha: y};
-                this.controlPoints.push(controlPoint);
-                this.controlPoints.sort((a, b) => a.x - b.x);
-                this.draw();
+                this.addControlPointAt(x, y);
                 checkDragStart(e);
             } else if (e.button === 1) { // Middle click
                 let indexToDelete = -1;
                 for (let i = 1; i < this.controlPoints.length - 1; i++) {
                     const controlPoint = this.controlPoints[i];
-                    const dx = controlPoint.x * this.canvas.width - e.offsetX;
+                    const dx = controlPoint.stop * this.canvas.width - e.offsetX;
                     const dy = controlPoint.alpha * this.canvas.height - e.offsetY;
-                    if (Math.sqrt(dx * dx + dy * dy) < 5) {
+                    if (Math.sqrt(dx * dx + dy * dy) < this.controlPointSize) {
                         indexToDelete = i;
                         break;
                     }
@@ -240,11 +278,11 @@ export class TransparencyEditor {
                 } else if (this.dragIndex === this.controlPoints.length - 1) {
                     this.controlPoints[this.dragIndex].alpha = y;
                 } else {
-                    this.controlPoints[this.dragIndex].x = x;
+                    this.controlPoints[this.dragIndex].stop = x;
                     this.controlPoints[this.dragIndex].alpha = y;
                 }
 
-                this.controlPoints.sort((a, b) => a.x - b.x);
+                this.sortControlPoints();
                 this.draw();
             }
         });
@@ -253,7 +291,35 @@ export class TransparencyEditor {
             this.isDragging = false;
             this.dragIndex = -1;
         });
+
+        this.canvas.addEventListener("mouseleave", (e) => {
+            if (this.isDragging) {
+                const controlPoint = this.controlPoints[this.dragIndex];
+                if (e.offsetY < 0) {
+                    controlPoint.alpha = 0;
+                } else if (e.offsetY > this.canvas.height) {
+                    controlPoint.alpha = 1;
+                }
+
+                if (e.offsetX < 0 && this.dragIndex > 0) {
+                    controlPoint.stop = (this.dragIndex + 1) * Number.EPSILON;
+                } else if (e.offsetX > this.canvas.width && this.dragIndex < this.controlPoints.length - 1) {
+                    controlPoint.stop = 1 - (this.controlPoints.length - this.dragIndex) * Number.EPSILON;
+                }
+
+                this.isDragging = false;
+                this.dragIndex = -1;
+
+                this.sortControlPoints();
+                this.draw();
+            }
+        });
     }
+}
+
+export interface ColorStop {
+    stop: number;
+    rgb: string;
 }
 
 export class ColorMapEditor {
@@ -262,15 +328,16 @@ export class ColorMapEditor {
     private ctx: CanvasRenderingContext2D;
     private colorRange: d3Scale.ScaleLinear<string, string>;
 
-    private colorMap: Array<{ x: number, rgb: string }>;
+    private colorMap: Array<ColorStop>;
 
     private isDragging: boolean = false;
     private dragIndex: number = -1;
+    private controlPointSize: number = 5;
 
-    constructor(container: HTMLElement | string, colorMap: Array<{ x: number, rgb: string }> = [
-        {x: 0, rgb: 'blue'},
-        {x: 0.5, rgb: 'white'},
-        {x: 1, rgb: 'red'}
+    constructor(container: HTMLElement | string, colorMap: Array<ColorStop> = [
+        {stop: 0, rgb: 'blue'},
+        {stop: 0.5, rgb: 'white'},
+        {stop: 1, rgb: 'red'}
     ]) {
         if (container) {
             if (typeof (container) == "string") {
@@ -308,10 +375,10 @@ export class ColorMapEditor {
         this.ctx.strokeStyle = "black";
         for (let i = 0; i < this.colorMap.length; i++) {
             this.ctx.fillStyle = this.colorMap[i].rgb;
-            const x = this.colorMap[i].x * this.canvas.width;
+            const x = this.colorMap[i].stop * this.canvas.width;
             const y = 0.5 * this.canvas.height;
             this.ctx.beginPath();
-            this.ctx.arc(x, y, 5, 0, 2 * Math.PI);
+            this.ctx.arc(x, y, this.controlPointSize, 0, 2 * Math.PI);
             this.ctx.fill();
             this.ctx.stroke();
         }
@@ -319,7 +386,7 @@ export class ColorMapEditor {
 
     private updateColorRange() {
         this.colorRange = d3Scale.scaleLinear<string, number>()
-            .domain(this.colorMap.map(entry => entry.x))
+            .domain(this.colorMap.map(entry => entry.stop))
             .range(this.colorMap.map(entry => entry.rgb))
             .interpolate(d3Interpolate.interpolateRgb)
     }
@@ -329,9 +396,9 @@ export class ColorMapEditor {
             this.dragIndex = -1;
             for (let i = 0; i < this.colorMap.length; i++) {
                 const stop = this.colorMap[i];
-                const dx = stop.x * this.canvas.width - e.offsetX;
+                const dx = stop.stop * this.canvas.width - e.offsetX;
                 const dy = 0.5 * this.canvas.height - e.offsetY;
-                if (Math.sqrt(dx * dx + dy * dy) < 5) {
+                if (Math.sqrt(dx * dx + dy * dy) < this.controlPointSize) {
                     this.dragIndex = i;
                     this.isDragging = true;
                     break;
@@ -351,9 +418,9 @@ export class ColorMapEditor {
             const x = Math.max(0, Math.min(1, e.offsetX / this.canvas.width));
             if (e.button === 0) { // Left click
                 const rgb = this.colorRange(x);
-                const stop = {x, rgb};
+                const stop = {stop: x, rgb};
                 this.colorMap.push(stop);
-                this.colorMap.sort((a, b) => a.x - b.x);
+                this.colorMap.sort((a, b) => a.stop - b.stop);
                 this.updateColorRange();
                 this.draw();
                 checkDragStart(e);
@@ -361,9 +428,9 @@ export class ColorMapEditor {
                 let indexToDelete = -1;
                 for (let i = 1; i < this.colorMap.length - 1; i++) {
                     const stop = this.colorMap[i];
-                    const dx = stop.x * this.canvas.width - e.offsetX;
+                    const dx = stop.stop * this.canvas.width - e.offsetX;
                     const dy = 0.5 * this.canvas.height - e.offsetY;
-                    if (Math.sqrt(dx * dx + dy * dy) < 5) {
+                    if (Math.sqrt(dx * dx + dy * dy) < this.controlPointSize) {
                         indexToDelete = i;
                         break;
                     }
@@ -381,10 +448,10 @@ export class ColorMapEditor {
                 const x = Math.max(0, Math.min(1, e.offsetX / this.canvas.width));
 
                 if (this.dragIndex !== 0 && this.dragIndex !== this.colorMap.length - 1) {
-                    this.colorMap[this.dragIndex].x = x;
+                    this.colorMap[this.dragIndex].stop = x;
                 }
 
-                this.colorMap.sort((a, b) => a.x - b.x);
+                this.colorMap.sort((a, b) => a.stop - b.stop);
                 this.updateColorRange();
                 this.draw();
             }
